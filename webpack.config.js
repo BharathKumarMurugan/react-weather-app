4be6e75a1da3a0bfeea5695cdb169006e823b67a
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2022-01-21 12:50:48 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css|\.scss$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    host: "localhost",
+    port: 7070,
+    compress: true,
+  },
+};
